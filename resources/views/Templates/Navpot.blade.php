29f5e8e1baac9c84607dbe64f6e1e0270e82e0b4
lgarin211 <lagustinus211@gmail.com>
2024-07-11 10:04:12 +0700

    modified:   resources/views/Templates/Navpot.blade.php

--- resources/views/Templates/Navpot.blade.php
+++ resources/views/Templates/Navpot.blade.php
@@ -4,20 +4,20 @@
         class="dropdown-toggle">Inventory <i class="bi bi-arrow-down-square"></i> </a>
     <ul class="collapse list-unstyled" id="inventorySubmenu">
         <li class="nav-item">
-            <a class="nav-link ml-2" href="#">Barang masuk</a>
+            <a class="nav-link ml-2" href="{{ route('employe') }}">Barang masuk</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link ml-2" href="#">Data Barang</a>
+            <a class="nav-link ml-2" href="{{ route('employe') }}">Data Barang</a>
         </li>
     </ul>
     <a class="nav-link" href="#penjualanSubmenu" data-toggle="collapse" aria-expanded="false"
         class="dropdown-toggle">Penjualan <i class="bi bi-arrow-down-square"></i> </a>
     <ul class="collapse list-unstyled" id="penjualanSubmenu">
         <li class="nav-item">
-            <a class="nav-link ml-2" href="#"> Kasir</a>
+            <a class="nav-link ml-2" href="{{ route('kasir') }}"> Kasir</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link ml-2" href="#"> Reture</a>
+            <a class="nav-link ml-2" href="{{ route('kasir') }}"> Reture</a>
         </li>
     </ul>
     <a class="nav-link" href="#accountSubmenu" data-toggle="collapse" aria-expanded="false"
@@ -27,7 +27,7 @@
             <a class="nav-link ml-2" href="#"> Profile</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link ml-2" href="#"> Sign out</a>
+            <a class="nav-link ml-2" href="{{ route('login') }}"> Sign out</a>
         </li>
     </ul>
 </nav>
