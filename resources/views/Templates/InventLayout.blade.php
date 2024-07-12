e4f200191d5c0e0d671530cae23cc1b3377f32e2
lgarin211 <lagustinus211@gmail.com>
2024-07-12 16:25:08 +0700

    new file:   public/LTE/adminlte.css 	new file:   public/LTE/adminlte.min.css 	modified:   resources/views/Templates/InventLayout.blade.php

--- resources/views/Templates/InventLayout.blade.php
+++ resources/views/Templates/InventLayout.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sidebar Example</title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    {{-- <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet"> --}}
+    <link rel="stylesheet" href="{{ url('LTE/adminlte.min.css') }}">
     <style>
         body {
             font-family: 'Arial', sans-serif;
